bbf73354e259e5af1899af52b0b6440db7419809
Aditrishivany <154952935+Aditrishivany@users.noreply.github.com>
2026-01-20 15:22:57 +0530

    Episode 10 - Align test with DosageCalculator method

--- dosage-calculator/src/main/java/com/healthtech/dosage/DosageCalculator.java
+++ dosage-calculator/src/main/java/com/healthtech/dosage/DosageCalculator.java
@@ -2,7 +2,7 @@ package com.healthtech.dosage;
 
 public class DosageCalculator {
 
-    public int calculateDose(int weightKg) {
+    public int calculateDosage(int weightKg) {
         return weightKg * 10;
     }
 }
